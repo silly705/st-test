3d77141400fd3c176c2956b215706de677dc6d68
silly705 <spysmother@gmail.com>
2024-08-03 04:29:48 -0400

    Update index.js
    I hope this works

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import { extension_settings, getContext, loadExtensionSettings } from "../../extensions.js";
 
 //You'll likely need to import some other functions from the main script
-import { saveSettingsDebounced, eventSource, event_types } from "../../../script.js";
+import { saveSettingsDebounced, eventSource, event_types } from "../../../../script.js";
 
 // Keep track of where your extension is located, name should match repo name
 const extensionName = "st-extension-example";
