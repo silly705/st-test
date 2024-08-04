bd33bf101367bd2d89ec3830408c34e64316346c
callanb5 <140440498+callanb5@users.noreply.github.com>
2024-08-03 21:13:32 -0400

    fixing stuff

--- scripts/autocomplete/AutoCompleteSecondaryNameResult.js
+++
@@ -1,6 +0,0 @@
-import { AutoCompleteNameResult } from './AutoCompleteNameResult.js';
-
-export class AutoCompleteSecondaryNameResult extends AutoCompleteNameResult {
-    /**@type {boolean}*/ isRequired = false;
-    /**@type {boolean}*/ forceMatch = true;
-}
--- scripts/slash-commands/SlashCommandClosureResult.js
+++
@@ -1,9 +0,0 @@
-export class SlashCommandClosureResult {
-    /**@type {boolean}*/ interrupt = false;
-    /**@type {string}*/ pipe;
-    /**@type {boolean}*/ isAborted = false;
-    /**@type {boolean}*/ isQuietlyAborted = false;
-    /**@type {string}*/ abortReason;
-    /**@type {boolean}*/ isError = false;
-    /**@type {string}*/ errorMessage;
-}
